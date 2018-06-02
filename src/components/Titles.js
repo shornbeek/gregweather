a3e91bffdb501d378626b09e89169ef69e5d5274
Stephen Hornbeek <hbeeken@hotmail.com>
2018-06-01 17:53:35 -0700

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+	render () {
+		return (
+		<div> 
+		<h1>Gregs Weather App</h1>
+		<p> Find out the Temp So you won't be to hot or to cold...</p>
+		</div>
+
+	   );
+	}
+};
+
+export default Titles;
